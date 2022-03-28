07bcfea056ad5e6a292a66170753d8467eb059a3
PC 13 <arindahafidah@gmail.com>
2022-03-28 01:42:07 -0700

    percobaan2

---
+++ src/D_MelakukanOperasiBit.java
@@ -0,0 +1,13 @@
+public class D_MelakukanOperasiBit {
+    public static void main(String[] args) {
+        int x = 5, y = 6;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("x&y="+(x&y));
+        System.out.println("x|y="+(x|y));
+        System.out.println("x^y="+(x^y));
+    }
+    /**
+     * contoh MelakukanOperasiBit
+     */
+}
